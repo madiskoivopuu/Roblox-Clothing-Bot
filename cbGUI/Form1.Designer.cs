3554e1a4812c5e2144d4c7e68865c2e187a548da
SparklyCatTF2 <32608971+SparklyCatTF2@users.noreply.github.com>
2018-11-11 11:05:19 +0200

    Add Clothing Bot files via upload
    This clothing bot is still half finished, I have to add the uploading feature and fix other issues

---
+++ cbGUI/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace cbGUI_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 101);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(594, 303);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 61);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(52, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Shirts";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(129, 61);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(62, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "T-Shirts";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(70, 61);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(53, 17);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Pants";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(267, 59);
+            this.textBox1.MaxLength = 8;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Start Page";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(385, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Stop Page";
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox2.Location = new System.Drawing.Point(388, 58);
+            this.textBox2.MaxLength = 8;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(116, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Upload";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 416);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Clothing Bot by Sparkly";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ cbGUI/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace cbGUI_CSharp
+{
+    public partial class Form1 : Form
+    {
+        //Global Variables
+        int g_iStartPage = 0;
+        int g_iStopPage = 1;
+        int g_iDownloadType; // Download type for a certain asset 12-Shirts, 13-TShirts, 14-Pants
+        string g_sAssetType; // String can contain Pants, Shirts or T-Shirts
+        int g_iItemCount; // Amount of assets downloaded
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartDownload();
+        }
+
+        private void StartDownload()
+        {
+            if(g_iDownloadType == 0)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("BOT | Cannot download without any asset type selected.");
+                return;
+            }
+
+            listBox1.Items.Clear();
+            listBox1.Items.Add("BOT | Download starting.");
+            DisableAllButtons();
+
+            //HTTP stuff
+            for (int i = g_iStartPage; i <= g_iStopPage;)
+            {
+                listBox1.Items.Clear();
+                String msgString = "BOT | Downloading assets from catalog page " + i;
+                listBox1.Items.Add(msgString);
+                ++i;
+                HttpClient client = new HttpClient();
+                try
+                {
+                    String urlPost1 = "https://search.roblox.com/catalog/json?Category=3&SortType=2&Subcategory=" + g_iDownloadType + "&AggregationFrequency=3&PageNumber=" + i + "&CatalogContext=1"; // Downloads clothes based on bestselling->past week
+                    HttpResponseMessage response = client.GetAsync(urlPost1).Result;
+                    response.EnsureSuccessStatusCode();
+                    HttpContent content = response.Content;
+
+                    String sContent = content.ReadAsStringAsync().Result;
+                    dynamic dynJson = JsonConvert.DeserializeObject(sContent);
+                    foreach (var item in dynJson)
+                    {
+                        ++g_iItemCount;
+                        String urlPost2 = "https://assetgame.roblox.com/asset/?id=" + item.AssetId;
+                        try
+                        {
+                            response = client.GetAsync(urlPost2).Result;
+                            content = response.Content;
+                            sContent = content.ReadAsStringAsync().Result;
+
+                            int pFrom = sContent.IndexOf("<url>") + "<url>".Length;
+                            int pTo = sContent.LastIndexOf("</url>");
+
+                            if (pTo - pFrom < 0)
+                            {
+                                String errString = "BOT | FAILURE - Item " + g_iItemCount + " - " + item.AssetId + " - " + item.Name;
+                                listBox1.Items.Add(errString);
+                            }
+                            else
+                            {
+                                String result = sContent.Substring(pFrom, pTo - pFrom);
+
+                                try
+                                {
+                                    String urlPost3 = result;
+
+                                    String assetName = item.Name;
+                                    String encodedName = EncodeTo64(assetName);
+                                    String filepath = Environment.CurrentDirectory + "\\Files\\" + g_sAssetType + "\\" + encodedName + ".png";
+
+                                    WebClient client2 = new WebClient();
+                                    client2.DownloadFile(result, filepath);
+
+                                    String success = "BOT | SUCCESS - Item " + g_iItemCount + " - " + item.AssetId + " - " + item.Name;
+                                    listBox1.Items.Add(success);
+                                }
+                                catch (HttpRequestException)
+                                {
+                                    String errString = "BOT | FAILURE - " + g_iItemCount + " - " + item.AssetId + " - " + item.Name;
+                                    listBox1.Items.Add(errString);
+                                }
+                            }
+                        }
+                        catch (HttpRequestException exception)
+                        {
+                            listBox1.Items.Add("BOT | Failed to download (stage 2).");
+                            String errString = "BOT ERROR: " + exception.Message;
+                            listBox1.Items.Add(errString);
+                            client.Dispose();
+                            return;
+                        }
+                    }
+                }
+                catch (HttpRequestException exception)
+                {
+                    listBox1.Items.Add("BOT | Failed to download (stage 1).");
+                    String errString = "BOT ERROR: " + exception.Message;
+                    listBox1.Items.Add(errString);
+                    client.Dispose();
+                    return;
+                }
+                client.Dispose();
+                EnableAllButtons();
+                g_iItemCount = 0;
+            }
+        }
+
+        private void DisableAllButtons()
+        {
+            button1.Enabled = false;
+        }
+        private void EnableAllButtons()
+        {
+            button1.Enabled = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) //Shirts
+        {
+            if(checkBox1.Checked)
+            {
+                g_iDownloadType = 12;
+                checkBox2.Enabled = false;
+                checkBox3.Enabled = false;
+                g_sAssetType = "Shirts";
+            }
+            else
+            {
+                g_iDownloadType = 0;
+                checkBox2.Enabled = true;
+                checkBox3.Enabled = true;
+                g_sAssetType = "";
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e) //T-Shirts
+        {
+            if (checkBox2.Checked)
+            {
+                g_iDownloadType = 13;
+                checkBox1.Enabled = false;
+                checkBox3.Enabled = false;
+                g_sAssetType = "T-Shirts";
+            }
+            else
+            {
+                g_iDownloadType = 0;
+                checkBox1.Enabled = true;
+                checkBox3.Enabled = true;
+                g_sAssetType = "";
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e) //Pants
+        {
+            if (checkBox3.Checked)
+            {
+                g_iDownloadType = 14;
+                checkBox1.Enabled = false;
+                checkBox2.Enabled = false;
+                g_sAssetType = "Pants";
+            }
+            else
+            {
+                g_iDownloadType = 0;
+                checkBox1.Enabled = true;
+                checkBox2.Enabled = true;
+                g_sAssetType = "";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) // Start Page textbox
+        {
+            if (textBox1.Text != "")
+            {
+                g_iStartPage = Convert.ToInt32(textBox1.Text);
+            }
+            else
+            {
+                g_iStartPage = 1;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) // Stop Page textbox
+        {
+            if(textBox2.Text != "")
+            {
+                g_iStopPage = Convert.ToInt32(textBox2.Text);
+            }
+            else
+            {
+                g_iStopPage = 1;
+            }
+        }
+
+        // Stocks by me
+
+        // Stocks by other people
+
+        static public string EncodeTo64(string toEncode)
+        {
+
+            byte[] toEncodeAsBytes
+
+                  = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+
+            string returnValue
+
+                  = System.Convert.ToBase64String(toEncodeAsBytes);
+
+            returnValue = returnValue.Replace("/", "-");
+
+            return returnValue;
+
+        }
+
+        static public string DecodeFrom64(string encodedData)
+        {
+
+            encodedData = encodedData.Replace("-", "/");
+
+            byte[] encodedDataAsBytes
+
+                = System.Convert.FromBase64String(encodedData);
+
+            string returnValue =
+
+               System.Text.ASCIIEncoding.ASCII.GetString(encodedDataAsBytes);
+
+            return returnValue;
+
+        }
+
+        /*public static string Replace(string original, char replacement, params char[] replaceables)
+        {
+            StringBuilder builder = new StringBuilder(original.Length);
+            HashSet<char> replaceable = new HashSet<char>(replaceables);
+            foreach (Char character in original)
+            {
+                if (replaceable.Contains(character))
+                    builder.Append(replacement);
+                else
+                    builder.Append(character);
+            }
+            return builder.ToString();
+        }*/
+    }
+}
